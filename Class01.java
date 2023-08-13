ddeb40e37040b74867ea7d85dd474f403d24f537
X <x@x-ThinkCentre-M800>
2023-08-13 01:04:38 -0600

    Init repository, added hello world

---
+++ Class01.java
@@ -0,0 +1,14 @@
+public class Class01{
+    public static void main(String[] args){
+
+        System.out.println("Hello world!");
+
+        test(5);
+    }
+
+    public static void test(int nInput){
+        for(int i = 0; i < nInput; i++){
+            System.out.println(i);
+        }
+    }
+}
